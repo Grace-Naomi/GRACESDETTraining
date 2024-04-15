22852e6b1de265958065e44c32286fda87d5c5e7
Grace-Naomi <gracenaomi117@gmail.com>
2024-04-15 19:58:10 +0530

    Fibonacci Series

---
+++ src/test/java/JirTasks/FibonacciSeries.java
@@ -0,0 +1,27 @@
+package JirTasks;
+
+import java.util.Scanner;
+
+public class FibonacciSeries {
+
+
+        public static void main(String args[])
+        {
+            Scanner scanner=new Scanner(System.in);
+            System.out.println("enter the count");
+            int count=scanner.nextInt();
+
+            int n1=0,n2=1,n3,i;
+            System.out.print(n1+" "+n2); //print 0 and 1
+
+            for(i=2;i<count;++i)
+            {
+                n3=n1+n2;
+                System.out.print(" "+n3);
+                n1=n2;
+                n2=n3;
+            }
+
+        }
+
+}
