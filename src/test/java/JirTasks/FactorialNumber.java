8036ff80862ce76b53b3cffcd90c610f4758ad66
Grace-Naomi <gracenaomi117@gmail.com>
2024-04-15 19:42:31 +0530

    factorial Number

---
+++ src/test/java/JirTasks/FactorialNumber.java
@@ -0,0 +1,19 @@
+package JirTasks;
+
+import java.util.Scanner;
+
+public class FactorialNumber {
+    public static void main(String args[]){
+
+        int i, fact=1;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a number");
+        int number = scanner.nextInt();//It is the number to calculate factorial
+
+        for(i=1;i<=number;i++){
+            fact=fact*i;
+        }
+        System.out.println("Factorial of "+number+" is: "+fact);
+    }
+}
