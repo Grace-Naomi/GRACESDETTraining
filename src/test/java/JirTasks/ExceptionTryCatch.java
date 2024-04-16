62adb753332fcb48c7a6f773620292aeb3cf5707
Grace-Naomi <gracenaomi117@gmail.com>
2024-04-16 21:55:06 +0530

    Exception Try Catch Block

---
+++ src/test/java/JirTasks/ExceptionTryCatch.java
@@ -0,0 +1,17 @@
+package JirTasks;
+
+public class ExceptionTryCatch {
+
+    public static void main(String[] args) {
+
+        int b, a = 5;
+        try {
+            b = a / 0;
+            System.out.println(b);
+        } catch (ArithmeticException ae){
+            ae.printStackTrace();
+        }
+
+        System.out.println("Testing");
+    }
+}
